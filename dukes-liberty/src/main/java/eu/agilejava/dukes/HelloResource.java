7f223608b83870e43509b6e0754d15cb92d2947f
Ivar Grimstad <ivar.grimstad@gmail.com>
2019-02-06 10:17:37 +0100

    Minor adjustment to liberty demo!

--- dukes-liberty/src/main/java/eu/agilejava/dukes/HelloResource.java
+++ dukes-liberty/src/main/java/eu/agilejava/dukes/HelloResource.java
@@ -23,16 +23,14 @@
  */
 package eu.agilejava.dukes;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.metrics.annotation.Metered;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.eclipse.microprofile.metrics.annotation.Counted;
-import org.eclipse.microprofile.metrics.annotation.Metered;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 /**
  *
